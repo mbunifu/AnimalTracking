b17bf8823e9d4e50f5b862b518ed6f8b176e9aa6
Kashingi <moriskashing74@gmail.com>
2024-12-09 09:39:38 +0300

    Changes added.

--- src/app/population-health/population-health.module.ts
+++ src/app/population-health/population-health.module.ts
@@ -13,11 +13,13 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
+import { DistributionComponent } from './distribution/distribution.component';
 
 
 @NgModule({
   declarations: [
-    PopulationComponent
+    PopulationComponent,
+    DistributionComponent
   ],
   imports: [
     CommonModule,
