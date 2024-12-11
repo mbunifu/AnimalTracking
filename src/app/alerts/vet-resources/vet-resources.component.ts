62d898b417148e0b38aeb68b08b0cb33a158b079
Adefranky <155418329+Adefranky@users.noreply.github.com>
2024-12-11 13:08:49 +0300

    vet-resources

--- src/app/alerts/vet-resources/vet-resources.component.ts
+++ src/app/alerts/vet-resources/vet-resources.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VetResourcesComponent implements OnInit {
 
+  vetResources: { name: string; link: string }[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loadResources();
   }
 
+  loadResources(): void {
+    this.vetResources = [
+      { name: 'Kenya Veterinary Board (KVB)', link: 'https://www.kenyavetboard.or.ke' },
+      { name: 'Kenya Veterinary Association (KVA)', link: 'https://kenyavetassociation.com' },
+      { name: 'VSF Germany Kenya Projects', link: 'https://www.vsfg.org/kenya' },
+      { name: 'Department of Veterinary Services', link: 'https://kilimo.go.ke' },
+    ];
+  }
 }
