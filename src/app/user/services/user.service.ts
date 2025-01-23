df5e2067b80f5b0730f48a4065eee0fdd33f5dcc
[jeremychege] <chege1040@gmail.com>
2025-01-23 17:53:40 +0300

    connected admins endpoint

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,12 +1,21 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment.prod";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpclient:HttpClient) { }
+  constructor(private http:HttpClient) { }
 
-  public register
+
+  getAdminUsers(): Observable <any[]>{
+
+    const usersUrl = `${environment.baseUrl}/api/v1/admin/get/all`;
+
+    return this.http.get<any[]>(usersUrl);
+  }
+
 }
